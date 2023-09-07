8a550e98659b60dbdeb23cf2cc5f86764c84269e
Shoaib <shoaibrochi9@gmail.com>
2023-09-07 18:36:43 +0600

    add strings

---
+++ js/strings.js
@@ -0,0 +1,6 @@
+//both '' and "" used to declare strings but has some differences
+"The book name is \"The Alchemist\"" //The book name is "The Alchemist"
+'The book name is \"The Alchemist\"' //same 
+'The book name is \'The Alchemist\'' //same
+var x="This is how to print single backslash \\";
+console.log(x); //This is how to print backslash \
