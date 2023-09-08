3aadb289d6dcf47fab0ec04ee234003853275a6f
Shoaib <shoaibrochi9@gmail.com>
2023-09-09 00:40:36 +0600

    update string

--- js/strings.js
+++ js/strings.js
@@ -4,3 +4,38 @@
 'The book name is \'The Alchemist\'' //same
 var x="This is how to print single backslash \\";
 console.log(x); //This is how to print backslash \
+//tab space
+y="This is tab space in \t between"
+console.log(y) //This is tab space in 	 between
+//new line
+z="this is \n new line";
+console.log(z); 
+//this is 
+// new line
+
+//Concatenation
+var c= x+y;
+console.log(c) //This is how to print single backslash \This is tab space in 	 between
+
+//length
+c.length //length is a property
+
+//String indexing
+var x="Shoaib"; 
+x[0]; // 'S' //indexing starts with 0
+x[3]; // 'a'
+x[7]; // undefined
+
+//Methods of string
+//Upper-lower-case
+x.toLocaleUpperCase(); // SHOAIB
+console.log(x); //Shoaib // value will not be changed
+z=x.toLocaleUpperCase(); // SHOAIB // Value can be stored
+
+//for .toLowerCase same as .toUpperCase
+
+//Trim method
+x=" Shaoib  Rochi   " //' Shaoib   Rochi  '
+x.trim() // 'Shaoib   Rochi' //removes unnecessary spaces
+//Again this method only giving a result , we need to store it for using it
+z=x.trim(); // 'Shaoib   Rochi'
