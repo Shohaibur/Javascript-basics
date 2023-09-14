b2a5597198cf7f1e105a6ff3d0506fdc61450466
Shoaib <shoaibrochi9@gmail.com>
2023-09-14 19:54:15 +0600

    add template literal

---
+++ template-literal.js
@@ -0,0 +1,20 @@
+//Es6 feature
+// Backtick `` 
+// Used to output multiple line of strings
+
+let s = `Hello
+there 
+how are 
+you`;
+console.log(s) ;
+//Hello
+//there 
+//how are 
+//you
+
+let age = 26;
+console.log(`his age is ${age}`); //his age is 26
+
+let a = 34
+let b = 67
+console.log(`${a} + ${b} = ${ a + b }`); //34 + 67 = 101
