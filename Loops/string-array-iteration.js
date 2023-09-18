4a192e2909b96cac5f13fc83fe9318c4c9e4e200
Shoaib <shoaibrochi9@gmail.com>
2023-09-19 01:12:42 +0600

    added: string-array-iteration

---
+++ Loops/string-array-iteration.js
@@ -0,0 +1,15 @@
+console.log("string-array-iteration file contents:")
+//string
+let n="Shohaibur";
+let len=n.length;
+for (i=0; i<len; i++){
+  console.log("index: "+i)
+  console.log(n[i]);
+}
+
+// array
+let colors=["red","yellow","green"];
+for (i=0; i<colors.length; i++){
+  console.log("Index for food: "+i)
+  console.log(colors[i]);
+}
