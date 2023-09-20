0d6b812883cb24cce291ff24a12c04941344995c
Shoaib <shoaibrochi9@gmail.com>
2023-09-20 16:33:56 +0600

    added: Functions

---
+++ Functions/intro.js
@@ -0,0 +1,13 @@
+//there are 2 types of functions
+// 1, built in and 2. user defined
+//alert() , console.log() are build in functions
+
+//user defined functions
+//declaration of function
+function myFunction(){
+  console.log("Hello");
+  console.log("my name is Shoaib");
+}
+//execution of function
+myFunction();
+myFunction(); //function is reusable
