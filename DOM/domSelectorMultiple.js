f770b4c458785ab0bd7236ea06125a6028c69c9a
Shoaib <shoaibrochi9@gmail.com>
2023-09-28 03:11:08 +0600

    update: domSelectorMultiple

--- DOM/domSelectorMultiple.js
+++ DOM/domSelectorMultiple.js
@@ -1,1 +1,49 @@
-console.log("domSelectorMultiple file contents:")
+console.log("domSelectorMultiple file contents : ");
+//Multi selector
+// document.getElementByClassName()
+let list = document.getElementsByClassName('sample-class');
+list[0].style.background = 'red';
+list[0].style.color = 'yellow';
+list[0].style.padding = '10px';
+list[0].textContent = 'Hello World!';
+console.log(list[0]); //access first index with sample-class class
+
+//document.getElementByTagName()
+list = document.getElementsByTagName('li');
+console.log(list[0]);  //access 1st element 'C++' using indexing
+
+
+
+list= document.querySelector('ol').getElementsByTagName('li'); //list of li tags under ol
+console.log(list); 
+
+let lis = Array.from(list); //convert into array and print 
+lis.forEach(function(items){
+  console.log(items);
+})
+
+//documentQuerySelectorAll
+// id -> #
+// className -> .
+//tagname -> nothing
+list = document.querySelectorAll('.sample-class'); //all classes (node list)
+list = document.querySelectorAll('ol li'); //li tags under ol
+list.forEach(function(liitems){
+  console.log(liitems);
+});
+console.log(list);
+
+let liOdd = document.querySelectorAll('li:nth-child(odd)');
+let liEve = document.querySelectorAll('li:nth-child(even)');
+liOdd.forEach(function(item){
+  item.style.background = 'grey';
+  item.style.color = 'white';
+});
+
+liEve.forEach(function(item){
+  item.style.background = 'red';
+  item.style.color = 'white';
+});
+
+
+console.log(list); 
