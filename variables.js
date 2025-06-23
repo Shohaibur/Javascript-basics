a8a03a8db90ccb5d6e96d4d9e33eb3863db723e3
Shohaibur Rahman <102200830+Shohaibur@users.noreply.github.com>
2025-06-23 10:47:39 +0000

    add variables file

---
+++ variables.js
@@ -0,0 +1,7 @@
+const accId = "1245";
+let accName = "John"; 
+businessName = "Growtheon";
+let businessYear;
+let numberOfClients = 200;
+
+console.table([accId,accName,businessName,businessYear,numberOfClients]);
