063e41cafc3a45e9a179c013bd51994349eae40f
Shohaibur Rahman <102200830+Shohaibur@users.noreply.github.com>
2025-07-08 07:43:00 +0000

    conversion type add

---
+++ conversion.js
@@ -0,0 +1,35 @@
+const a = "5";
+const A = Number(a);
+console.log(A);
+console.log(typeof(A));
+console.log("\n");
+
+const b = "5jk";
+const B = Number(b);
+console.log(B);
+console.log(typeof(B));
+console.log("\n");
+
+const c= undefined;
+const C=Number(c);
+console.log(C);
+console.log(typeof(C));
+console.log("\n");
+
+const d=null;
+const D=Number(d);
+console.log(D);
+console.log(typeof(D));
+console.log("\n");
+
+const e=NaN
+const E=Number(e);
+console.log(E);
+console.log(typeof(E));
+console.log("\n")
+
+const f= true;
+const F=Number(f);
+console.log(F);
+console.log(typeof(F));
+
