1d2a8385178cceb1595b814379fe985ae199f393
Shohaibur Rahman <102200830+Shohaibur@users.noreply.github.com>
2025-07-14 08:37:29 +0000

    add comparison js file

---
+++ comparison.js
@@ -0,0 +1,20 @@
+console.log (2>10); // false
+console.log (null == undefined); // true
+console.log (null === undefined); // false
+console.log (null > undefined); // false
+console.log (null == 0); // false
+console.log (null >= 0); // true
+console.log (null < 0); // false
+console.log (null > 0); // false    
+console.log (null <= 0); // true
+console.log (undefined == 0); // false
+console.log (undefined >= 0); // false
+console.log (undefined < 0); // false
+console.log (undefined > 0); // false
+console.log (undefined <= 0); // false
+console.log (undefined == null); // true
+console.log ("2" == 2); // true
+console.log ("2" === 2); // false
+console.log ("2" > 2); // false
+console.log ("2" < 2); // false
+console.log ("2" == 2); // true
